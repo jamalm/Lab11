d4007b364bb6c88b81ea7a3323ad9625ab44c9e2
jamalm <bluedocjamal@hotmail.co.uk>
2015-12-02 14:35:39 +0000

    Fixed file reading dependency - now allows direct pull of key into program

--- verify-boto-and-keys.py
+++ verify-boto-and-keys.py
@@ -1,25 +1,24 @@
 #separate the amazon key into 2 variables
 import boto
 import requests
+import urllib2
 
 def main():
 	verify()
 	version()
 def verify():
-	#curl command retrived key and put into a file previously called key(in same directory)
-	f = open('key','r')
-	key = f.read()
+	#curl command retrived key
+	key = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+	response = key.read()		
 	#character to look for when splitting key
 	split = ':'
-	i = key.find(split)
+	i = response.find(split)
 	# splits key in half
-	word1 = key[:i]
-	word2 = key[i+len(split):]
+	word1 = response[:i]
+	word2 = response[i+len(split):]
 	# Prints two new variables 
 	print word1
 	print word2
-	##close key file
-	f.close()
 def version():
 	# prints current version of boto
 	print boto.Version
